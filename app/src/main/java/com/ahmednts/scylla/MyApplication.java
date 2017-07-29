3d36457663e98263ceb01c01bc11ce317e1d0952
Ahmed Hashem <ahmedhashemnts@gmail.com>
2017-07-29 14:48:39 +0200

    init

---
+++ app/src/main/java/com/ahmednts/scylla/MyApplication.java
@@ -0,0 +1,49 @@
+package com.ahmednts.scylla;
+
+import android.app.Application;
+import android.os.StrictMode;
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by AhmedNTS on 7/29/2017.
+ */
+
+public class MyApplication extends Application {
+
+  private static MyApplication instance;
+
+  public static MyApplication getInstance() {
+    return instance;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return;
+    }
+    LeakCanary.install(this);
+
+    instance = this;
+
+    setStrictMode();
+  }
+
+  void setStrictMode() {
+    if (BuildConfig.DEBUG) {
+      StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads()
+          .detectDiskWrites()
+          .detectNetwork()   // or .detectAll() for all detectable problems
+          .penaltyLog()
+          .build());
+      StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects()
+          .detectLeakedClosableObjects()
+          .penaltyLog()
+          .penaltyDeath()
+          .build());
+    }
+    super.onCreate();
+  }
+}
