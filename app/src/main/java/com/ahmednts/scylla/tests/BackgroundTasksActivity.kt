dbbc3d888d99fd1d2bb6b785c06ae8ec4c8bfc40
Ahmed Hashem <ahmedhashemnts@gmail.com>
2018-07-30 20:35:24 +0200

    init

---
+++ app/src/main/java/com/ahmednts/scylla/tests/BackgroundTasksActivity.kt
@@ -0,0 +1,15 @@
+package com.ahmednts.scylla.tests
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ahmednts.scylla.R
+
+class BackgroundTasksActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_background_tasks)
+
+
+  }
+}
