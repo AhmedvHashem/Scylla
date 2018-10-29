33bd7acb11ee7b97699fade9e9d3fb34918e91f3
ahmednts <ahmedhashemnts@gmail.com>
2018-10-29 13:24:26 +0200

    build on PR and TAG only

---
+++ app/src/main/java/com/ahmednts/scylla/Main3Activity.kt
@@ -0,0 +1,14 @@
+package com.ahmednts.scylla
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main3.*
+
+class Main3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        setSupportActionBar(toolbar)
+    }
+}
