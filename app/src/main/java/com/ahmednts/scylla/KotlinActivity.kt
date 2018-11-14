2e465e25d5a41bf0e95389dd5a31107fa92821e5
ahmednts <ahmedhashemnts@gmail.com>
2018-11-14 15:08:06 +0200

    added square coding style which applies to both java and kotlin (SquareAndroid)

---
+++ app/src/main/java/com/ahmednts/scylla/KotlinActivity.kt
@@ -0,0 +1,12 @@
+package com.ahmednts.scylla
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_kotlin)
+  }
+}
