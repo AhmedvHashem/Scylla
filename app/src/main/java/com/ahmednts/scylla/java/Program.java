cbbf123086c26ab9d0523194ef976a84d4423f2e
Ahmed Hashem <ahmedhashemnts@gmail.com>
2019-12-01 18:24:04 +0200

    testing date diffs

--- app/src/main/java/com/ahmednts/scylla/java/Program.java
+++ app/src/main/java/com/ahmednts/scylla/java/Program.java
@@ -3,5 +3,14 @@ package com.ahmednts.scylla.java;
 public class Program {
   public static void main(String[] args) {
 
+    long enterDate = 1575212400000L;
+    long exitDate = 1575213600000L;
+
+    long diff = exitDate - enterDate;
+    int diffMinutes = (int) (diff / (60 * 1000));
+
+    int count = Math.round(diffMinutes / 25);
+
+    System.out.println(count);
   }
 }
