c3428f7da8d7df1da94830adadb160ea9e3c589a
Ahmed Hashem <ahmedhashemnts@gmail.com>
2020-06-14 20:39:51 +0200

    playing with animation

--- app/src/main/java/com/ahmednts/scylla/design/DesignActivity.kt
+++ app/src/main/java/com/ahmednts/scylla/design/DesignActivity.kt
@@ -1,69 +1,94 @@
 package com.ahmednts.scylla.design
 
-import android.annotation.TargetApi
-import android.graphics.Color
-import android.os.Build
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.animation.TranslateAnimation
 import com.ahmednts.scylla.R
-import kotlinx.android.synthetic.main.activity_design.fab
-import kotlinx.android.synthetic.main.activity_design.toolbar
+import kotlinx.android.synthetic.main.activity_design.*
 
 class DesignActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_design)
-    setSupportActionBar(toolbar)
 
-    drawBehindStatusBar()
-    setStatusBarStyle(false)
-
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null)
-          .show()
-    }
-  }
-
-  private fun drawBehindStatusBar() {
-    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-
-    if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
-      if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-        window.statusBarColor = Color.TRANSPARENT
-//        window.navigationBarColor = Color.TRANSPARENT
-      } else {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
-            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-        )
+    buttonUp.setOnClickListener {
+      viewToAnimate.post {
+        viewToAnimate.showAlertUp(3000)
       }
-
-//      window.setFlags(
-//          WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-//          WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-//      )
     }
 
-    findViewById<ViewGroup>(android.R.id.content)?.getChildAt(0)
-        ?.fitsSystemWindows = true
+    buttonDown.setOnClickListener {
+      viewToAnimate.showAlertDown(3000)
+    }
+  }
+}
+
+fun View.showAlertUp(duration: Long) {
+  clearAnimation()
+  show()
+  post {
+    val animate = TranslateAnimation(
+        0f,
+        0f,
+        0f,
+        -height.toFloat())
+
+    animate.duration = 500
+    animate.fillAfter = true
+    startAnimation(animate)
   }
 
-  @TargetApi(Build.VERSION_CODES.M)
-  private fun setStatusBarStyle(isLight: Boolean) {
-    if (isLight)
-      window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
-          View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-    else
-      window.decorView.systemUiVisibility = window.decorView.systemUiVisibility and
-          View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+  postDelayed({
+    val animate = TranslateAnimation(
+        0f,
+        0f,
+        -height.toFloat(),
+        0f)
+
+    animate.duration = 300
+    animate.fillAfter = true
+    startAnimation(animate)
+  }, duration)
+}
+
+fun View.showAlertDown(duration: Long) {
+  clearAnimation()
+  show()
+  post {
+    val animate = TranslateAnimation(
+        0f,
+        0f,
+        0f,
+        height.toFloat())
+
+    animate.duration = 500
+    animate.fillAfter = true
+    startAnimation(animate)
   }
+
+  postDelayed({
+    val animate = TranslateAnimation(
+        0f,
+        0f,
+        height.toFloat(),
+        0f)
+
+    animate.duration = 300
+    animate.fillAfter = true
+    startAnimation(animate)
+  }, duration)
+}
+
+fun View.show() {
+  this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+  this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+  this.visibility = View.GONE
 }
