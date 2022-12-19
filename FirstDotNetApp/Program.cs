36ec514cc420c4a6da70592b1ba580156eaa3237
Ahmed Hashem <ahmed.hashem@bevy.com>
2022-12-19 07:05:39 +0200

    asdasd

--- FirstDotNetApp/Program.cs
+++ FirstDotNetApp/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World! C#");
+namespace FirstDotNetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! C# Program");
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World! C#");
