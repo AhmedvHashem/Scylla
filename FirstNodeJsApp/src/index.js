70c6f6f36cceb1b9ed0aa200491f7923195d771c
Ahmed Hashem <ahmed.hashem@bevy.com>
2023-02-03 08:21:28 +0200

    xzxz

--- FirstNodeJsApp/src/index.js
+++ FirstNodeJsApp/src/index.js
@@ -1,6 +1,9 @@
 const app = require("express")();
 
-app.get("/", (req, res) => res.json({ message: "Docker is easy 🐳" }));
+var count = 0;
+app.get("/", (req, res) =>
+  res.json({ message: "Docker is easy 🐳", count: ++count })
+);
 
 const port = process.env.PORT || 8080;
 
