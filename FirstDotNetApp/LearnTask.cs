3e2198cb3e33fb1ed7025c696c50a1d739ff8e89
Ahmed Hashem <ahmed.hashem@bevy.com>
2023-08-22 15:41:41 +0300

    asd

--- FirstDotNetApp/LearnTask.cs
+++ FirstDotNetApp/LearnTask.cs
@@ -2,7 +2,7 @@ namespace FirstDotNetApp;
 
 public static class LearnTask
 {
-    public static void Main(string[] args)
+    public static void Maasdin(string[] args)
     {
         Console.WriteLine("Hello World! C# Program");
         Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
@@ -23,5 +23,26 @@ public static class LearnTask
         Console.WriteLine("Main before wait");
         t.Wait();
         Console.WriteLine("Main after wait");
+    }
+
+    public static async Task Main(string[] args)
+    {
+        // part 1
+        Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
+        var client = new HttpClient();
+        Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
+        var task = client.GetStringAsync("http://google.com");
+        Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
+
+        var a = 0;
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            a += i;
+        }
+
+        Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
+        var page = await task;
+        // part 2
+        Console.WriteLine("Environment.CurrentManagedThreadId: " + Environment.CurrentManagedThreadId);
     }
 }
