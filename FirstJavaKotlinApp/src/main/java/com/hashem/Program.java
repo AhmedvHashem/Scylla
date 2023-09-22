4ffef11fc8cc9ac6d57bed3546dea01f4a013928
Ahmed Hashem <ahmedvhashem@gmail.com>
2023-09-22 06:01:20 +0300

    awd

--- FirstJavaKotlinApp/src/main/java/com/hashem/Program.java
+++ FirstJavaKotlinApp/src/main/java/com/hashem/Program.java
@@ -5,3 +5,19 @@ public class Program {
         System.out.println("Hello world!");
     }
 }
+
+class Test {
+    static void swap(int a, int b) {
+        int t = a;
+        a = b;
+        b = t;
+    }
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+
+        System.out.println("pre:  x = " + x + " y = " + y);
+        swap(x, y);
+        System.out.println("post: x = " + x + " y = " + y);
+    }
+}
