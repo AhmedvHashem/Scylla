f7e0d588480dfa054fd9abc5b37f9907a254ae29
Ahmed Hashem <ahmedvhashem@gmail.com>
2023-10-04 16:02:02 +0300

    awd

--- FirstAndroidApp/android/src/main/java/com/hashem/android/view/MainActivity.kt
+++ FirstAndroidApp/android/src/main/java/com/hashem/android/view/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Sensor.TYPE_AMBIENT_TEMPERATURE
+
         MySingleton.getInstance {
             findViewById<TextView>(R.id.btn).text = "Hi from Singleton"
         }
