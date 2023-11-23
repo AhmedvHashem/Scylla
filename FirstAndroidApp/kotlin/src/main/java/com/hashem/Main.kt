f55d544a82105158ab375f5b98f0a72a6a089eb9
Ahmed Hashem <ahmed.hashem@bevy.com>
2023-11-24 00:40:32 +0200

    qwe

--- FirstAndroidApp/kotlin/src/main/java/com/hashem/Main.kt
+++ FirstAndroidApp/kotlin/src/main/java/com/hashem/Main.kt
@@ -5,7 +5,39 @@ fun main(args: Array<String>) {
 //    val result = computeGameState("Bob", "Anna", listOf("Bob", "Anna", "Bob"))
 //    println(result)
 
-    val points = listOf(listOf(1, 2), listOf(3, 4), listOf(0, 0), listOf(5, 5))
-    val result = boundingRectangle(points)
-    println(result)  // Output: [0, 0, 5, 5]
+
+    data class PersonV2(
+        val name: String,
+        val data: CharArray,
+    ){
+        fun asd() {
+            data.asSequence()
+        }
+
+    }
+
+    val p1 = PersonV2("Amanda", charArrayOf('A', 'm', 'a', 'n', 'd', 'a'))
+    val p2 = PersonV2("Amanda", charArrayOf('A', 'm', 'a', 'n', 'd', 'a'))
+    println(p1.equals(p2))
+
+}
+
+fun reverse(x: Int): Int {
+    var x = x
+    var reversed = 0
+    while (x != 0) {
+        val lastDigit = x % 10
+        reversed = reversed * 10 + lastDigit
+        x /= 10
+    }
+    return reversed
+}
+
+
+private const val UNKNOWN_ERROR = 0
+
+class Test {
+    companion object {
+        private const val UNKNOWN_ERROR = 0
+    }
 }
