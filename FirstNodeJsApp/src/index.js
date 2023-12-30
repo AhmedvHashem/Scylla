fe05b4f3afd85ab9e68fa7a9b3770877674fbc14
Ahmed Hashem <ahmed.hashem@bevy.com>
2023-12-30 22:34:27 +0200

    test memcached

--- FirstNodeJsApp/src/index.js
+++ FirstNodeJsApp/src/index.js
@@ -1,11 +1,11 @@
 const app = require("express")();
 
+const port = process.env.PORT || 8080;
+
 var count = 0;
 app.get("/", (req, res) =>
   res.json({ message: "Docker is easy 🐳", count: ++count })
 );
-
-const port = process.env.PORT || 8080;
 
 app.listen(port, () =>
   console.log(`app listening on http://localhost:${port}`)
