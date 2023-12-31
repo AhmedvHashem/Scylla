97668a1e25ad8f6562c6ba8deae10882af321ff3
Ahmed Hashem <ahmed.hashem@bevy.com>
2023-12-31 02:20:37 +0200

    asd

--- FirstDjangoProject/books/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from books.views import AuthorViewSet, BookViewSet
-
-router = routers.DefaultRouter()
-router.register(r"authors", AuthorViewSet, basename="authors")
-router.register(r"books", BookViewSet, basename="books")
-
-urlpatterns = router.urls
--- FirstDjangoProject/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from members import views
-
-urlpatterns = [
-    path("members/", views.members, name="members"),
-    path("members/<slug:slug>/", views.details, name="details"),
-]
