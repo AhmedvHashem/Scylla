04daf9b4124321ffd82e5c6bace733f6dc3be32e
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-12-22 21:11:36 +0200

    awdawdawdaqwdqwd

---
+++ Android/FirstAndroidApp/app/src/test/java/com/hashem/firstandroidapp/JavaTest.java
@@ -0,0 +1,13 @@
+package com.hashem.firstandroidapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JavaTest {
+    @Test
+    public void test() {
+        System.out.println("Hello from JavaTest");
+
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
---
+++ Android/FirstAndroidApp/app/src/test/java/com/hashem/firstandroidapp/KotlinTest.kt
@@ -0,0 +1,14 @@
+package com.hashem.firstandroidapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class KotlinTest {
+    @Test
+    fun test() {
+        println("Hello from KotlinTest")
+
+        assertEquals(4, 2 + 2)
+    }
+}
