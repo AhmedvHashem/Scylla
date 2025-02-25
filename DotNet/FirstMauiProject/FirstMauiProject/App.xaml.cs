2adfdc150ebe1894c82bf644aee1db6214c5c31c
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-25 05:12:34 +0200

    awdawdawd

---
+++ DotNet/FirstMauiProject/FirstMauiProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstMauiProject;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ DotNet/FirstMauiProject/FirstMauiProject/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstMauiProject;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
