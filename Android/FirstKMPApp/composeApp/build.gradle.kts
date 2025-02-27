a48e244d9c79b1fc4387106166f94b0ea1946c80
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-02-27 03:09:48 +0200

    Changing permission of gradlew

--- Android/FirstKMPApp/composeApp/build.gradle.kts
+++ Android/FirstKMPApp/composeApp/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -11,14 +9,13 @@ plugins {
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
     
     listOf(
-        iosX64(),
+//        iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
