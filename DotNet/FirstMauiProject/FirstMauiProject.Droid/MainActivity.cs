645d02a5f638b564cec853a8ea117376ab2fc0b0
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-03-02 16:08:57 +0200

    fsdf

--- DotNet/FirstMauiProject/FirstMauiProject.Droid/MainActivity.cs
+++ DotNet/FirstMauiProject/FirstMauiProject.Droid/MainActivity.cs
@@ -1,10 +1,20 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
 
 namespace FirstMauiProject.Droid;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        var testString = "Hello from Android!";
+        Toast.MakeText(BaseContext, testString, ToastLength.Long)?.Show();
+    }
 }
--- iOS/FirstiOSApp/FirstiOSApp/ContentView.swift
+++ iOS/FirstiOSApp/FirstiOSApp/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!").bold()
-        }
-        .padding()
+        Text("Hello, world!").padding().bold()
+        MetalView().frame(height: 300)
     }
 }
 
