908aa67029a773a9f5bc5a19ad4f7e93145a018b
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-03-05 05:57:50 +0200

    fsdf

---
+++ Android/FirstCMPApp/androidApp/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.compose)
+}
+
+android {
+    namespace = "com.hashem.firstcmpapp"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.hashem.firstcmpapp"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":composeApp"))
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.activity.compose)
+}
---
+++ Android/FirstCMPApp/androidApp/src/main/java/com/hashem/firstcmpapp/AndroidActivity.kt
@@ -0,0 +1,3 @@
+package com.hashem.firstcmpapp
+
+class AndroidActivity : MainActivity()
